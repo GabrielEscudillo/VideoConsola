b3c609fab0840c49d2ae8ce7dd5cc54c35f25915
GabrielEscudillo <gabrielescudillo@gmail.com>
2023-10-26 23:26:21 +0200

    creacion

---
+++ README.md
@@ -0,0 +1,54 @@
+# Nintendo Switch
+
+La nintendo Switch es una consola de videojuegos desarrollada por Nintendo, es considerada una consola híbrida. Puede ser utilizada de sobremesa con la unidad principal insertada en una estación de acoplamiento que permite conectarla a un televisor y también puede ser extraida y utilizada como una tablet a través de su pantalla táctil o incluso se puede colocar en una superficie y ser manipulada con los mandos extraibles. 
+
+## Contenido
+
+- [Introducción](#introducción)
+- [Características](#características)
+- [Tecnologías](#tecnologías-utilizadas) 
+- [Captura de Pantalla](#capturas-de-pantalla)
+ 
+## Introducción
+
+En el segundo proyecto nos han pedido crear una videoconsola con HTML, CSS y JS, nos han permitido elegir la que quisieramos y entre tantas opciones escogí crear esta debido a su complejidad para poder conocer un poco más de estos lenguajes además de ponerme el reto a mi mismo de crear algo que no fuera tan sencillo. 
+
+## Características
+
+Este proyecto consta de dos documentos HTML, uno de inicio, donde se muestra una invitación a disfrutar de la Nintendo Switch y que nos muestra un boton de acceso que nos lleva a la siguiente página donde se encuentra la videoconsola. 
+Se puede observar que esta consta de dos mandos laterales, cada uno cuenta con el boton correspondiente y todos los botones tienen una función. 
+Las funciones añadidas son: 
+- Start: Enciende/apaga la videoconsola
+- Next: Pasa a la siguiente animación 
+- Previous: Permite retroceder a la animación anterior. 
+
+Todo el diseño ha sido creado con divs, no se han insertado imagenes para simular botones ni flechas. En la siguiente imagen, se pueden observar los principales divs que componen la videoconsola:
+
+<img src="./img/divs.png" alt="divs" width="450"/>
+
+En este momento, ya se había hecho el formato principal,
+
+<img src="./img/medio.png" alt="boxes" width="450"/>
+
+En la siguiente imagen podemos observar el resultado final.
+
+<img src="./img/resultado.png" alt="final" width="450"/>
+
+## Tecnologías Utilizadas
+
+<img src="./img/tecnologias.png" alt="lenguajes" width="200"/>
+ 
+ ## Futuras mejoras
+
+En la vida, todo se puede mejorar. Algunas cosas que me gustaría mejorar son: 
+ - Añadir efectos de sonido.
+ - Programar algún tipo de juego que se pueda ejecutar con los botones de esta  videoconsola.
+ - Añadir más funciones. 
+
+## Capturas de Pantalla
+
+<img src="./img/startimg.png" alt="release" width=""/>
+
+<img src="./img/game1.png" alt="drawing" width=""/>
+
+<img src="./img/game2.png" alt="drawing" width=""/>
---
+++ js/main.js
@@ -0,0 +1,56 @@
+document.addEventListener('DOMContentLoaded', function() {
+    let onOffButton = document.getElementById('onOffButton');
+    let gamepad = document.querySelector('.gamepad');
+    let isGifVisible = false; // Variable para rastrear si el GIF está visible
+    let originalGamepadStyle = gamepad.style.cssText; // Guarda las propiedades CSS originales
+    let imageIndex = 0; // Índice para rastrear qué imagen se debe mostrar
+
+    // Lista de nombres de archivos de imágenes en tu carpeta "img"
+    let imageFiles = ['../img/animals.gif', '../img/jump.gif', '../img/mario.gif', '../img/mariokart.gif'];
+
+    onOffButton.addEventListener('click', function() {
+        // Si el GIF está visible, quítalo y restaura las propiedades originales
+        if (isGifVisible) {
+            gamepad.style.backgroundImage = 'none';
+            gamepad.style.cssText = originalGamepadStyle;
+        } else {
+            // Si el GIF no está visible, muéstralo y marca el estado como encendido
+            gamepad.style.backgroundImage = 'url("../img/start.gif")';
+        }
+        // Cambia el estado de la visibilidad del GIF
+        isGifVisible = !isGifVisible;
+    });
+
+    document.querySelector('.botonRight').addEventListener('click', cambiarImagen);
+    document.querySelector('.botonDown').addEventListener('click', cambiarImagen);
+    document.querySelector('.botona').addEventListener('click', cambiarImagen);
+    document.querySelector('.botonb').addEventListener('click', cambiarImagen);
+    document.querySelector('.JoystickRight').addEventListener('click', cambiarImagen);
+    document.querySelector('.botonMas').addEventListener('click', cambiarImagen);
+    document.querySelector('.botonMas2').addEventListener('click', cambiarImagen);
+
+    function cambiarImagen() {
+        // Cambia la imagen de fondo a la siguiente imagen en la lista
+        imageIndex++;
+        if (imageIndex >= imageFiles.length) {
+            imageIndex = 0; // Vuelve al inicio si llega al final de la lista
+        }
+        gamepad.style.backgroundImage = 'url("../img/' + imageFiles[imageIndex] + '")';
+    };
+
+    document.querySelector('.botonMenos').addEventListener('click', CambiarImagen);
+    document.querySelector('.botonx').addEventListener('click', CambiarImagen);
+    document.querySelector('.botony').addEventListener('click', CambiarImagen);
+    document.querySelector('.botonUp').addEventListener('click', CambiarImagen);
+    document.querySelector('.botonLeft').addEventListener('click', CambiarImagen);
+    document.querySelector('.JoystickLeft').addEventListener('click', CambiarImagen);
+
+    function CambiarImagen() {
+        // Cambia la imagen de fondo a la imagen anterior en la lista
+        imageIndex--;
+        if (imageIndex < 0) {
+            imageIndex = imageFiles.length - 1; // Vuelve al final si está en el inicio de la lista
+        }
+        gamepad.style.backgroundImage = 'url("../img/' + imageFiles[imageIndex] + '")';
+    };
+}); 
